e1ab15d20a41ebe2e5914018ca7181f94bb24b09
Karina Selezneva <karinabukhartseva@yandex.com>
2026-01-22 15:02:27 +0300

    feat: add structured logging with slog and improve unsubscribe logic

--- internal/broker/broker.go
+++ internal/broker/broker.go
@@ -1,6 +1,10 @@
 package broker
 
-import "sync"
+import (
+	"log/slog"
+	"os"
+	"sync"
+)
 
 // Определи структуру Broker:
 // 1. Внутри должно быть поле для мапы (ключ — строка, значение — слайс каналов).
@@ -9,13 +13,25 @@ import "sync"
 type Broker struct {
 	mu          sync.RWMutex
 	subsByTopic map[string][]chan string
+	logger      *slog.Logger
 }
 
 // Напиши конструктор NewBroker():
 // Функция, которая возвращает инициализированную структуру (не забудь сделать make для мапы!).
 func NewBroker() *Broker {
+	// Создаем логгер, который пишет только важную инфу в одну строку
+	handler := slog.NewTextHandler(os.Stdout, &slog.HandlerOptions{
+		ReplaceAttr: func(groups []string, a slog.Attr) slog.Attr {
+			// Опционально: убираем время для локальной разработки, чтобы не мусорило
+			if a.Key == slog.TimeKey {
+				return slog.Attr{}
+			}
+			return a
+		},
+	})
 	return &Broker{
 		subsByTopic: make(map[string][]chan string),
+		logger:      slog.New(handler),
 	}
 }
 
@@ -32,6 +48,8 @@ func (b *Broker) Subscribe(topic string) chan string {
 	defer b.mu.Unlock()
 
 	b.subsByTopic[topic] = append(b.subsByTopic[topic], ch)
+
+	b.logger.Info("+New Subscribe", "topic", topic, "count", len(b.subsByTopic[topic]))
 
 	return ch
 }
@@ -51,6 +69,13 @@ func (b *Broker) Unsubscribe(topic string, ch chan string) {
 	for i, val := range subs {
 		if val == ch {
 			b.subsByTopic[topic] = append(subs[:i], subs[i+1:]...)
+
+			// Если подписчиков больше нет — удаляем топик целиком
+			if len(b.subsByTopic[topic]) == 0 {
+				delete(b.subsByTopic, topic)
+			}
+
+			b.logger.Info("-Unsubscribe", "topic", topic, "count", len(b.subsByTopic[topic]))
 
 			return
 		}
